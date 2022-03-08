598f771a90012e1797d3fffc5f21383cc655e10d
Misha <jennysgod@gmail.com>
2022-03-08 17:34:28 +0300

    add capitalize, test

---
+++ tests/capitalize.test.js
@@ -0,0 +1,8 @@
+import { strict as assert } from 'assert';
+import capitalize from '../src/capitalize.js';
+
+assert.equal(capitalize('hello'), 'Hello');
+
+assert.equal(capitalize(''), '');
+
+console.log('Все тесты пройдены!');
